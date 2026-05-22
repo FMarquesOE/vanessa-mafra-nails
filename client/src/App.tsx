1b8a85ec2fe152664c6312d7acb12c487d2aa7ee
FMarquesOE <felipemarques07@gmail.com>
2026-05-22 09:35:03 +0000

    Agora acho que foi!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Modelo1 from "./pages/modelo-1";
+import Modelo1 from "./pages/modelo-1.tsx";
 
 function Router() {
   return (
