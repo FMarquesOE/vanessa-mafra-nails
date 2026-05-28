30e31b2ff0bee617f595e0f080e4a26a14a00df3
FMarquesOE <felipemarques07@gmail.com>
2026-05-28 19:10:34 -0300

    Cores e Fonte

--- client/src/pages/modelo-1.tsx
+++ client/src/pages/modelo-1.tsx
@@ -241,7 +241,7 @@ function SectionLabel({
       <span
         style={{
           fontFamily: "Inter, sans-serif",
-          fontSize: 14,
+          fontSize: 1,
           fontWeight: 600,
           letterSpacing: "0.28em",
           textTransform: "uppercase",
