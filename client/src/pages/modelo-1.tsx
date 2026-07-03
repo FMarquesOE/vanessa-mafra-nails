752c17d2d540f6aeceacb8abcf51a5ff782d4668
FMarquesOE <felipemarques07@gmail.com>
2026-07-03 01:57:03 -0300

    Edição de texto e link de rodapé

--- client/src/pages/modelo-1.tsx
+++ client/src/pages/modelo-1.tsx
@@ -1612,11 +1612,11 @@ function Footer() {
           }}
         >
           © {CONFIG.anoFundacao} · Todos os direitos reservados<br />
-          Construção do site por
+          Construção do site por 
           <a href="https://wa.me/5521976822900?text=Ol%C3%A1%20FMarques%20TechSolu%C3%A7%C3%B5es!%20Quero%20saber%20mais%20sobre%20os%20servi%C3%A7os%20da%20empresa." 
           target="_blank" 
           rel="noopener noreferrer" 
-          style={{ color: c.salvia, textDecoration: "bold" }}>FMarques TechSoluções</a>
+          style={{ color: c.texto, textDecoration: "none", fontWeight: "bold" }}>FMarques TechSoluções</a>
         </span>
       </div>
     </footer>
